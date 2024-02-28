f190fa38a0f2f8c2cf6baad6127aafa465e1b4dd
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-02-28 05:36:45 -0300

    Aplicação estruturada no Padrão (Pattern) MVVM - Model-View-ViewModel.

--- app/src/main/java/br/com/fiap/calculodejuros/MainActivity.kt
+++ app/src/main/java/br/com/fiap/calculodejuros/MainActivity.kt
@@ -3,40 +3,11 @@ package br.com.fiap.calculodejuros
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.com.fiap.calculodejuros.calculos.calcularJuros
-import br.com.fiap.calculodejuros.calculos.calcularMontante
-import br.com.fiap.calculodejuros.components.CaixaDeEntrada
-import br.com.fiap.calculodejuros.components.CardResultado
+import br.com.fiap.calculodejuros.juros.JurosScreen
 import br.com.fiap.calculodejuros.ui.theme.CalculoDeJurosTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,112 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun JurosScreen() {
-
-    // Variáveis para garantir o estado dos campos de entrada para toda renderização
-    var capital by remember { mutableStateOf("") }
-    var taxa by remember { mutableStateOf("") }
-    var tempo by remember { mutableStateOf("") }
-    var juros by remember { mutableStateOf(0.0) }
-    var montante by remember { mutableStateOf(0.0) }
-
-    Box(
-        modifier = Modifier.padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Column() {
-            Text(
-                text = "Cálculo de Juros Simples",
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = 20.sp,
-                color = Color.Red,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            // Formulário para entrada de dados
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Dados do Investimento",
-                        fontWeight = FontWeight.Bold
-                    )
-
-                    // Utilizando o componente CaixaDeEntrada para o valor do investimento
-                    CaixaDeEntrada(
-                        value = capital, // Variável que controla o estado a partir do componente pai
-                        placeholder = "Quanto deseja investir?",
-                        label = "Valor do investimento",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        keyboardType = KeyboardType.Decimal,
-                        atualizarValor = {
-                            capital = it //Leia-se: capital recebe uma string it (valor atualizado na digitação)
-                        }
-                    )
-
-                    // Utilizando o componente CaixaDeEntrada para a taxa de juros
-                    CaixaDeEntrada(
-                        value = taxa,
-                        placeholder = "Qual a taxa de juros mensal?",
-                        label = "Taxa de juros mensal",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        keyboardType = KeyboardType.Decimal,
-                        atualizarValor = {
-                            taxa = it
-                        }
-                    )
-
-                    // Utilizando o componente CaixaDeEntrada para o período de aplicação
-                    CaixaDeEntrada(
-                        value = tempo,
-                        placeholder = "Qual o período de aplicação em meses?",
-                        label = "Período em meses",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        keyboardType = KeyboardType.Decimal,
-                        atualizarValor = {
-                            tempo = it
-                        }
-                    )
-
-                    // Botão para calcular
-                    Button(
-                        onClick = {
-                            juros = calcularJuros(
-                                capital = capital.toDouble(),
-                                taxa = taxa.toDouble(),
-                                tempo = tempo.toDouble()
-                            )
-                            montante = calcularMontante(
-                                capital = capital.toDouble(),
-                                juros = juros
-                            )
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 32.dp)
-                    ) {
-                        Text(text = "CALCULAR")
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Utilizando o componente CardResultado para exibir o resultado
-            CardResultado(
-                juros = juros,
-                montante = montante
-            )
-
-        }
-    }
-}
---
+++ app/src/main/java/br/com/fiap/calculodejuros/juros/jurosScreenViewModel.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.calculodejuros.juros
+
+import androidx.lifecycle.ViewModel
+
+// Classe que representa o ViewModel da tela de cálculo de juros
+class jurosScreenViewModel: ViewModel() {
+
+}
